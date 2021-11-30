365e1725ad4360a97416a54dbda44743ced6456e
Duncan Bauer <duncanebauer@gmail.com>
2021-11-30 01:13:08 -0800

    just ignore this

--- class_template.hpp
+++ class_template.hpp
@@ -31,61 +31,15 @@ class TemplateClass
         /*****************
          * Overloaded Operators
          ****************/
-        // Assignment operator
-        virtual TemplateClass& operator=(const TemplateClass& other)
-        {
-        }
 
         /*****************
          * Getters and Setters
          ****************/
-		int getExampleInt() const { return this->exampleInt; }
-		void setExampleInt() { this->exampleInt = _exampleInt; }
 
     private:
         /*****************
          * Variable Members
          ****************/
-		int exampleInt;
 };
 
 #endif
-
-
-
-private:
-	/*****************
-	 * Typedefs
-	 ****************/
-
-public:
-	/*****************
-	 * Constructors
-	 ****************/
-	// Default constructor
-	TemplateClass() = default;
-
-	// Parameterized constructors
-	
-	// Copy Constructor
-	// Don't deep copy textures / models
-	TemplateClass(const TemplateClass& other){}
-
-	// Move Constructor
-	TemplateClass(TemplateClass&& other){}
-
-	// Destructor
-	~TemplateClass() {}
-
-	/*****************
-	 * Overloaded Operators
-	 ****************/
-	 
-	/*****************
-	 * Getters and Setters
-	 ****************/
-
-private:
-	/*****************
-	 * Variable Members
-	 ****************/
